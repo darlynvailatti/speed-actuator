5770ac930891ca53ca953188469c2b18c587bca5
Darlyn Vailatti <darlynvailatti@gmail.com>
2021-01-24 15:46:52 -0300

    Update build-bootstrap

--- bin/build-bootstrap.sh
+++ bin/build-bootstrap.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
+echo "Building back-end..."
+yarn install
+yarn build
+
 echo "Building front-end..."
 cd admin/front-end
 yarn install
 yarn build
 
-echo "Building back-end..."
-cd ../../
-pwd
-yarn install
-yarn build
